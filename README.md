f1ce4cb0cd81b3d3f1ac870c6467af4d37747502
Yukai <caroyalqi@gmail.com>
2025-06-10 19:11:50 +0100

    update: enhance platform compatibility (win+linux )and add logging in simple_shared_control

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 继续阅读shared control的文献 搞清楚 PSC 找到具体的baseline、具体要解决的问题（就是 我引入了errP之后，我要用什么方法去实现共享控制） 然后确定下evaluation的metrics
+
+# TODO
+1 注意平台兼容性 当前 `src\shared_control\shared_control\simple_shared_control.py` 里面只支持了linux的键位 (已解决 现支持lin+win)
--- src/shared_control/setup.py
+++ src/shared_control/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'shared_control'
 
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')), # add this line to include launch files
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),  # add this line to include config files
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,7 +24,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'simple_shared_control = shared_control.simple_shared_control:main',
+            'simple_shared_control = shared_control.simple_shared_control:main', # entry point for the shared control node
+            'ros_tcp_server = ros_tcp_endpoint.default_server_endpoint:main',  # entry point for the ros_tcp_endpoint server
         ],
     },
 )
