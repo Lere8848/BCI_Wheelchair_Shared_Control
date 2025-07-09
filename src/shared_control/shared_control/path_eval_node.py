a9c1b1e07d9de043170070c5d8c3e8416d7e0892
Yukai <caroyalqi@gmail.com>
2025-07-09 23:41:18 +0100

    update: 添加1.5s逻辑 每次用户输入只持续1.5s 然后等待用户反应；添加danger stop逻辑

--- src/shared_control/shared_control/path_eval_node.py
+++ src/shared_control/shared_control/path_eval_node.py
@@ -1,27 +1,64 @@
 # path_eval_node.py
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Bool
-from std_msgs.msg import Int8MultiArray  # 可用简单数组表示三个方向
+from sensor_msgs.msg import LaserScan, Range
+from std_msgs.msg import Int8MultiArray, Bool
+import numpy as np
 
 class PathEvalNode(Node):
     def __init__(self):
         super().__init__('path_eval_node')
         self.pub = self.create_publisher(Int8MultiArray, '/path_options', 10)
+        self.sub = self.create_subscription(LaserScan, '/scan', self.lidar_callback, 10)
+        # self.sub = self.create_subscription(Range, '/ultrasonic_front', self.lidar_callback, 10)
+        self.danger_pub = self.create_publisher(Bool, '/danger_stop', 10)
+        self.lidar_ranges = []
         self.timer = self.create_timer(1.0, self.timer_callback)
-        self.get_logger().info('PathEvalNode initialized. Publishing path options every second.')
+        self.get_logger().info('PathEvalNode initialized with LIDAR.')
+
+    def lidar_callback(self, msg):
+        self.lidar_ranges = np.array(msg.ranges)
 
     def timer_callback(self):
-        # mock 可通方向：0=不可通，1=可通
-        # 此时只允许向右或向左走（前边是死路）
-        # 后面需要在这里加入实际的路径评估逻辑 
-        mock_state = [0, 1, 1]  # 左=1 前=0 右=1
+        if len(self.lidar_ranges) == 0:
+            return
 
-        msg = Int8MultiArray()
-        msg.data = mock_state
-        self.pub.publish(msg)
+        # lidar ranges 分为左、中、右三部分
+        # 假设 lidar_ranges 长度为 360，左 120，前 120，右 120
+        total_rays = len(self.lidar_ranges)
+        left_idx = range(0, total_rays // 3)
+        forward_idx = range(total_rays // 3, 2 * total_rays // 3)
+        right_idx = range(2 * total_rays // 3, total_rays)
 
-        self.get_logger().info(f'published path options: LEFT={mock_state[0]}, FORWARD={mock_state[1]}, RIGHT={mock_state[2]}')
+        # ultrasonic 冗余检测 防止 LIDAR 数据不全
+        # if self.front_ultrasonic < 0.5:
+        #    path[1] = 0 
+
+        def is_open(idx_range, threshold):
+            distances = self.lidar_ranges[idx_range]
+            distances = np.array(distances)
+            return np.any(distances > threshold) # 检测是否有足够的距离
+
+        path = [
+            int(is_open(left_idx, 0.5)), # left
+            int(is_open(forward_idx, 0.5)), # forward
+            int(is_open(right_idx, 0.5)) # right
+        ]
+
+        all_dists = np.array(self.lidar_ranges)
+        danger = np.any(all_dists < 0.35)
+
+        # path_options
+        path_msg = Int8MultiArray()
+        path_msg.data = path
+        self.pub.publish(path_msg)
+
+        # danger_stop
+        danger_msg = Bool()
+        danger_msg.data = bool(danger) # numpt的bool和ros2 msg bool 不兼容 需要转换
+        self.danger_pub.publish(danger_msg)
+
+        self.get_logger().info(f'/path_options: {path} | danger_stop: {danger}')
 
 def main(args=None):
     rclpy.init(args=args)
--- src/shared_control/shared_control/static_shared_control_fusion_node.py
+++ src/shared_control/shared_control/static_shared_control_fusion_node.py
@@ -1,8 +1,9 @@
 # fusion_node.py
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int8, Int8MultiArray
+from std_msgs.msg import Int8, Int8MultiArray, Bool
 from geometry_msgs.msg import Twist
+import time
 
 class FusionNode(Node):
     def __init__(self):
@@ -12,11 +13,20 @@ class FusionNode(Node):
         self.user_cmd_sub = self.create_subscription(Int8, '/user_cmd', self.user_cmd_callback, 10)
         self.path_opt_sub = self.create_subscription(Int8MultiArray, '/path_options', self.path_callback, 10)
 
+        self.danger = False
+        self.create_subscription(Bool, '/danger_stop', self.danger_callback, 10)
+
         self.path_options = [1, 1, 1]  # default all passable
 
     def path_callback(self, msg):
         if len(msg.data) == 3:
             self.path_options = msg.data
+
+    def danger_callback(self, msg):
+        self.danger = msg.data
+        if self.danger:
+            self.get_logger().warn('DANGER STOP TRIGGERED. Emergency brake!')
+            self.cmd_pub.publish(Twist()) # Stop the wheelchair immediately
 
     def user_cmd_callback(self, msg):
         direction = msg.data  # 0=left, 1=forward, 2=right
@@ -35,14 +45,21 @@ class FusionNode(Node):
                 twist.linear.x = 0.5
             elif direction == 2:  # right
                 twist.angular.z = 0.4
+            
+            self.cmd_pub.publish(twist)
             self.get_logger().info(f'Executing user intent: {["LEFT","FORWARD","RIGHT"][direction]}')
+        
+            # 每次控制持续1.5秒 延迟后发送停止
+            time.sleep(1.5)
+            self.get_logger().info('Executing finished')
+            stop_twist = Twist()
+            self.cmd_pub.publish(stop_twist)
+
         else:
             # Unsafe direction, stop
             self.get_logger().warn(f'Direction blocked: {["LEFT","FORWARD","RIGHT"][direction]}, stopping.')
-            twist.linear.x = 0.0
-            twist.angular.z = 0.0
+            self.cmd_pub.publish(Twist())
 
-        self.cmd_pub.publish(twist)
 
 def main(args=None):
     rclpy.init(args=args)
