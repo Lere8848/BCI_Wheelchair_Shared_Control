f98fccdbbc36e8c2938b7b344e83309d24a8d111
Yukai <caroyalqi@gmail.com>
2025-08-13 17:36:43 +0100

    integration: add ROS-TCP-Endpoint node in bci_integration_launch

--- src/shared_control/launch/bci_groundtruth_launch.py
+++ src/shared_control/launch/bci_groundtruth_launch.py
@@ -5,6 +5,14 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
+        # ROS-TCP-Endpoint
+        Node(
+            package='ros_tcp_endpoint',
+            executable='default_server_endpoint',
+            name='ros_tcp_endpoint',
+            parameters=[{'ROS_IP': '127.0.0.1'}]
+        ),
+
         # Ground truth intent input node
         Node(
             package='shared_control',
